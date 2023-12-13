16683773c1c7f98974057b7c6e7bd4101b6700a5
Zach Antunes <ztunes4@gmail.com>
2023-12-13 09:04:06 -0800

    initial files

---
+++ app.py
@@ -0,0 +1,43 @@
+import openai
+import re
+from flask import Flask, render_template, request
+
+# Authenticate with the API key
+openai.api_key = "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"
+
+# Create a Flask app
+app = Flask(__name__, template_folder='superheroapp/templates')
+
+# Define the home page route
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+# Define the results page route
+@app.route("/results", methods=["POST"])
+def results():
+    # Get the user input for superhero name
+    hero_name = request.form["hero_name"]
+
+    # Generate prompt for OpenAI API
+    prompt = f"Generate a brief synopsis about the origin story of a superhero named {hero_name}."
+    response = openai.Completion.create(
+        engine="davinci",
+        prompt=prompt,
+        max_tokens=60,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+
+    # Extract the generated synopsis from the OpenAI API response
+    synopsis = response.choices[0].text
+    synopsis = re.sub('[^0-9a-zA-Z\n\.\?!,]+', ' ', synopsis)
+    synopsis = synopsis.strip()
+
+    # Render the results page with the generated synopsis
+    return render_template("results.html", hero_name=hero_name, synopsis=synopsis)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
