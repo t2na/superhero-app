84000c32de0303b25f4985c74c9a73541e3af8e1
Zach Antunes <ztunes4@gmail.com>
2023-12-13 10:16:28 -0800

    working on the API

--- app.py
+++ app.py
@@ -1,16 +1,20 @@
+from dotenv import load_dotenv
+load_dotenv()  # This loads the variables from the .env file
+
 import os
 import openai
 import re
 from flask import Flask, render_template, request
 
-# Import the os module
-import os
-
 # Load the API key from an environment variable
-openai.api_key = os.getenv('OPENAI_API_KEY')
+openai_api_key = os.getenv('OPENAI_API_KEY')
+if openai_api_key is None:
+    raise ValueError("No OPENAI_API_KEY found in environment variables")
+
+openai.api_key = openai_api_key
 
 # Create a Flask app
-app = Flask(__name__, template_folder='superheroapp/templates')
+app = Flask(__name__, template_folder='templates')
 
 # Define the home page route
 @app.route("/")
@@ -22,26 +26,33 @@ def home():
 def results():
     # Get the user input for superhero name
     hero_name = request.form["hero_name"]
+    synopsis = ''
 
-    # Generate prompt for OpenAI API
-    prompt = f"Generate a brief synopsis about the origin story of a superhero named {hero_name}."
-    response = openai.Completion.create(
-        engine="davinci",
-        prompt=prompt,
-        max_tokens=60,
-        n=1,
-        stop=None,
-        temperature=0.5,
-    )
+    try:
+        # Generate prompt for OpenAI API
+        prompt = f"Generate a brief synopsis about the origin story of a superhero named {hero_name}."
+        response = openai.Completion.create(
+            engine="davinci",
+            prompt=prompt,
+            max_tokens=60,
+            n=1,
+            stop=None,
+            temperature=0.5,
+        )
+        
+        # Extract the generated synopsis from the OpenAI API response
+        synopsis = response.choices[0].text
+        synopsis = re.sub('[^0-9a-zA-Z\n\.\?!,]+', ' ', synopsis)
+        synopsis = synopsis.strip()
+        
+    except openai.RateLimitError as e:
+        print("Rate limit exceeded: ", e)
+        synopsis = "Error: Rate limit exceeded. Please try again later."
+        # You might want to implement additional logic here, such as a retry mechanism or logging
 
-    # Extract the generated synopsis from the OpenAI API response
-    synopsis = response.choices[0].text
-    synopsis = re.sub('[^0-9a-zA-Z\n\.\?!,]+', ' ', synopsis)
-    synopsis = synopsis.strip()
-
-    # Render the results page with the generated synopsis
+    # Render the results page with the generated synopsis or error message
     return render_template("results.html", hero_name=hero_name, synopsis=synopsis)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
-
